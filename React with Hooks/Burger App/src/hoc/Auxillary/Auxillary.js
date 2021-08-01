42bcf96bc0eea519de796f2a388a70aa9f9f9d0f
Smile040501 <singlamayank001@gmail.com>
2021-08-01 09:47:15 +0530

    Added submodules

--- React with Hooks/Burger App/src/hoc/Auxillary/Auxillary.js
+++
@@ -1,3 +0,0 @@
-export default function Auxillary(props) {
-    return props.children;
-}
--- React with Hooks/Burger App/src/redux/actions/index.js
+++
@@ -1,5 +0,0 @@
-export { addIngredient, removeIngredient, initIngredients } from "./burgerBuilder";
-
-export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
-
-export { auth, authLogout, setAuthRedirectPath, authCheckState } from "./auth";
