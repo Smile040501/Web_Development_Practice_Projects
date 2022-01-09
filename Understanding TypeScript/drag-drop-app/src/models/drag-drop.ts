b9889de57ada73d0ff28388d33797139cc8d9a43
Mayank Singla <singlamayank001@gmail.com>
2022-01-09 12:57:55 +0530

    Modified project to use TS namespaces

---
+++ Understanding TypeScript/drag-drop-app/src/models/drag-drop.ts
@@ -0,0 +1,16 @@
+// Drag & Drop Interfaces
+namespace App {
+    export interface Draggable {
+        dragStartHandler(event: DragEvent): void;
+        dragEndHandler(event: DragEvent): void;
+    }
+
+    export interface DragTarget {
+        // To signal browser and JS that the thing we are dragging something over is a valid drag target
+        dragOverHandler(event: DragEvent): void;
+        // To react to actual drop that happens
+        dropHandler(event: DragEvent): void;
+        // If user does nothing, we can revert the drag and visual updates
+        dragLeaveHandler(event: DragEvent): void;
+    }
+}
