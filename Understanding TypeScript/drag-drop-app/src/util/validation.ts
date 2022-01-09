1ed3b51cb438bd6733cc0a903f8d0f5d3307ade6
Mayank Singla <singlamayank001@gmail.com>
2022-01-09 14:07:14 +0530

    Modified project using ES6 imports/exports

--- Understanding TypeScript/drag-drop-app/src/util/validation.ts
+++ Understanding TypeScript/drag-drop-app/src/util/validation.ts
@@ -1,36 +1,34 @@
-namespace App {
-    // Validation
-    export interface Validatable {
-        value: string | number;
-        required?: boolean;
-        minLength?: number;
-        maxLength?: number;
-        min?: number;
-        max?: number;
-    }
+// Validation
+export interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
 
-    export function validate(validatableInput: Validatable) {
-        const { value } = validatableInput;
-        let isValid = true;
-        if (validatableInput.required) {
-            isValid &&= value.toString().trim().length !== 0;
-        }
-        if (typeof value === "string") {
-            if (validatableInput.minLength !== undefined) {
-                isValid &&= value.length >= validatableInput.minLength;
-            }
-            if (validatableInput.maxLength !== undefined) {
-                isValid &&= value.length <= validatableInput.maxLength;
-            }
-        }
-        if (typeof value === "number") {
-            if (validatableInput.min !== undefined) {
-                isValid &&= value >= validatableInput.min;
-            }
-            if (validatableInput.max !== undefined) {
-                isValid &&= value <= validatableInput.max;
-            }
-        }
-        return isValid;
+export function validate(validatableInput: Validatable) {
+    const { value } = validatableInput;
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid &&= value.toString().trim().length !== 0;
     }
+    if (typeof value === "string") {
+        if (validatableInput.minLength !== undefined) {
+            isValid &&= value.length >= validatableInput.minLength;
+        }
+        if (validatableInput.maxLength !== undefined) {
+            isValid &&= value.length <= validatableInput.maxLength;
+        }
+    }
+    if (typeof value === "number") {
+        if (validatableInput.min !== undefined) {
+            isValid &&= value >= validatableInput.min;
+        }
+        if (validatableInput.max !== undefined) {
+            isValid &&= value <= validatableInput.max;
+        }
+    }
+    return isValid;
 }
