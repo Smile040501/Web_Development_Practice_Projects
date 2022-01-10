8e67908c816cf2bf5f8e2e0ad6539b95acc0b4fd
Mayank Singla <singlamayank001@gmail.com>
2022-01-11 03:14:16 +0530

    Added todo-rest-api made using Node.js and TypeScript

---
+++ Understanding TypeScript/todo-rest-api/src/controllers/todos.ts
@@ -0,0 +1,49 @@
+import { RequestHandler } from "express";
+
+import { Todo } from "../models/todo";
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, _) => {
+    const { text } = req.body as { text: string };
+    const newTodo = new Todo(Math.random().toString(), text);
+    TODOS.push(newTodo);
+
+    res.status(201).json({
+        message: "Created the todo.",
+        createdTodo: newTodo,
+    });
+};
+
+export const getTodos: RequestHandler = (_, res, _2) => {
+    res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, _) => {
+    const todoId = req.params.id;
+
+    const updatedText = (req.body as { text: string }).text;
+
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+    if (todoIndex < 0) {
+        throw new Error("Could not find todo!");
+    }
+    TODOS[todoIndex] = new Todo(todoId, updatedText);
+
+    res.json({ message: "Updated!", updatedTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler = (req, res, _) => {
+    const todoId = req.params.id;
+
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+    if (todoIndex < 0) {
+        throw new Error("Could not find todo!");
+    }
+
+    TODOS.splice(todoIndex, 1);
+
+    res.json({ message: "Todo deleted!" });
+};
